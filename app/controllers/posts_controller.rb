a1aa6917f0b6000a9da33b950efd89023c47f5dc
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-08-25 16:36:15 +0700

    Create MVC Category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,13 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    if current_user.admin
+      @posts = Post.all
+    else
+      @user = User.find(current_user.id)
+      @posts = @user.posts.all
+    end
+
   end
 
   # GET /posts/1
