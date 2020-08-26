42585e1f4e6236e4b527030cae7f345fc0b7981d
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-08-26 17:36:19 +0700

    UI screen signin/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root to: "homes#index"
+  
+  root to: 'homes#index'
+
   devise_for :users
+  # controllers: {
+  #   omniauth_callbacks: 'omniauth_callbacks',
+  #   # sessions: 'users/sessions'
+  # }
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
 end
---
+++ db/migrate/20200826101146_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :image, :string
+    add_column :users, :fullname, :string
+  end
+end
