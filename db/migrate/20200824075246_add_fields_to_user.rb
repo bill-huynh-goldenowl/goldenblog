c206c50a9c76c9ee09f69bbb3ebb5b622a19dcbf
stewart <npcuong.011308@gmail.com>
2020-08-26 14:48:34 +0700

    Purge db

--- db/migrate/20200824075246_add_fields_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :image, :string
-    add_column :users, :password, :string
-    add_column :users, :fullname, :string
-  end
-end
