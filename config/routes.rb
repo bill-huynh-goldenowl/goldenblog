a1984f62de857b642b01dcd65e4655a6bd7f3a6f
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-08-28 13:38:40 +0700

    Basic handle feature/user-management

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   
   root to: 'homes#index'
+  get 'users/edit'
+  patch 'users/update'
 
   devise_for :users
   # controllers: {
   #   omniauth_callbacks: 'omniauth_callbacks',
   #   # sessions: 'users/sessions'
   # }
-  # devise_scope :user do
-  #   get '/users/sign_out' => 'devise/sessions#destroy'
-  # end
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
