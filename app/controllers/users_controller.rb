50e6d6e5bbaa4915beadaba060053842031324a2
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-08-28 14:13:43 +0700

    Feature-user-management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :fullname, :location)
+  end
 end
