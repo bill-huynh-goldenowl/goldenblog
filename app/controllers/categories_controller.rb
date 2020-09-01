5784eaa768318af80d27f488b696f0558c0b93a3
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-09-01 14:31:50 +0700

    Create controller Posts

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -61,13 +61,13 @@ before_action :set_category, only: [:show, :edit, :update, :destroy]
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:title, :description)
-    end
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 end
