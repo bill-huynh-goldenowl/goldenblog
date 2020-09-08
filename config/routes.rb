b99fa58ab46a5cb2884a84ad71fa1f5e7de682aa
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-09-07 20:48:20 -0700

    Refactor code define routing post

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
+  root to: 'homes#index'
   
   resources :posts
   resources :categories
-  post '/posts/approve/:id' => 'posts#approve', as: :approve_posts
-  post '/posts/disapprove/:id' => 'posts#disapprove', as: :disapprove_posts
-  root to: 'homes#index'
+  resources :posts do
+    post :approve
+    post :disapprove
+  end
+
   get 'users/index'
   get 'users/show'
   get 'users/edit'
+
+  
   get 'users/edit_password/:id' => 'users#edit_password', as: :edit_password
   post 'users/update_pwd/:id' => 'users#update_pwd', as: :update_pwd
-  
   patch 'users/update/:id' => 'users#update', as: :update
 
   devise_for :users
-  # controllers: {
-  #   omniauth_callbacks: 'omniauth_callbacks',
-  #   # sessions: 'users/sessions'
-  # }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
