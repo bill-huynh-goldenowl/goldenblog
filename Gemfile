2efec5b4f9560f3d8cd1493e7bd96c8b9362b2e1
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-09-14 15:51:53 +0700

    Search basic

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'rubocop-rails', require: false
 
 gem 'pagy'
 
+gem 'ransack'
+
 gem 'jquery-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,11 @@ class HomesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.approval
+    @posts = 
+    if params[:q]
+      Post.ransack(title_cont: params[:q]).result(distinct: true)
+    else
+      Post.approval
+    end
   end
 end
