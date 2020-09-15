e9c12285331bd9d574d78fc825e95abe5187b119
bill-huynh-goldenowl <bill.huynh.goldenowl@gmail.com>
2020-09-15 11:23:28 +0700

    Refactor code comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,9 +3,10 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :parent, optional: true, class_name: "Comment"
 
-  def comments
-    Comment.where(commentable: commentable, parent_id: id)
-  end
+  has_many :comments, class_name: "Comment", foreign_key: 'parent_id', dependent: :destroy
+  # def comments
+  #   Comment.where(commentable: commentable, parent_id: id)
+  # end
 
   def destroy
     update(user: nil, body: nil)
